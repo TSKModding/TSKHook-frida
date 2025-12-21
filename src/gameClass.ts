88120aa99a11937077506f5fa0b9e9c0c293d3e7
c0re100 <corehusky@gmail.com>
2025-12-22 02:40:14 +0800

    Implement netHttpGet method
    Workaround for Frida 17, it may fail to hook newer ART (Version >= 3611xxxxx)

--- src/gameClass.ts
+++ src/gameClass.ts
@@ -5,6 +5,7 @@ export let AssetBundleModule: Il2Cpp.Image;
 export let TextMeshPro: Il2Cpp.Image;
 export let UnityCoreModule: Il2Cpp.Image;
 export let TextRenderingModule: Il2Cpp.Image;
+export let SystemModule: Il2Cpp.Image;
 
 export let AssetBundle: Il2Cpp.Class;
 export let TMP_FontAsset: Il2Cpp.Class;
@@ -12,6 +13,8 @@ export let FontAsset: Il2Cpp.Class;
 export let SysByte: Il2Cpp.Class;
 export let SysFile: Il2Cpp.Class;
 export let SysBinaryReader: Il2Cpp.Class;
+export let SysClass: Il2Cpp.Class;
+export let SysReader: Il2Cpp.Class;
 
 export let AdvPage: Il2Cpp.Class;
 export let AdvBacklog: Il2Cpp.Class;
@@ -40,4 +43,8 @@ Il2Cpp.perform(() => {
     AdvDataManager = Csharp.class('Utage.AdvDataManager')
     LanguageManagerBase = Csharp.class('Utage.LanguageManagerBase')
     AdventureTitleBandView = Csharp.class('Utage.AdventureTitleBandView')
+
+    SystemModule = Il2Cpp.domain.assembly("System").image;
+    SysClass = SystemModule.class('System.Net.WebRequest')
+    SysReader = Il2Cpp.corlib.class("System.IO.StreamReader");
 });
--- src/translation.ts
+++ src/translation.ts
@@ -1,14 +1,15 @@
 import {config} from './config.js';
 import * as util from './util.js';
+
 export let chapterDicts: Record<string, Record<string, string>> = {};
 export let nameDicts: Record<string, string> = {};
 
 
 export async function Init() {
     const url1 = config.transGameNovelCharaNameUrl;
-    nameDicts = await util.androidhttpGet(url1);
+    nameDicts = await util.netHttpGet(url1);
     const url2 = config.transGameNovelCharaSubNameUrl;
-    let respData = await util.androidhttpGet(url2);
+    let respData = await util.netHttpGet(url2);
     Object.assign(nameDicts, respData);
 
     console.log('name translation loaded. Total:', Object.keys(nameDicts).length);
@@ -16,7 +17,7 @@ export async function Init() {
 
 export async function FetchChapterTranslation(label) {
     const url = config.transGameNovelChapterUrl(label);
-    chapterDicts[label] = await util.androidhttpGet(url);
+    chapterDicts[label] = await util.netHttpGet(url);
 
     console.log('chapter translation loaded. Total:', Object.keys(chapterDicts[label]).length);
 }
